4efb705aafcdf06a17ce7686a9ecf1c228f51032
Domagoj Dusic <dd8614@rit.edu>
2024-03-19 18:33:09 +0000

    Resolve "Creating UI Skeleton classes"

--- view/InfoUI.java
+++ view/InfoUI.java
@@ -1,5 +1,21 @@
 package view;
 
-public class InfoUI {
-    
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class InfoUI extends Application{
+    private Stage stage;
+    private Scene scene;
+
+    public void start(Stage stage) {
+        stage.setTitle("Info");
+
+        VBox root = new VBox(8);
+
+        scene = new Scene(root, 750, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
