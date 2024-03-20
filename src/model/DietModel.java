db325e57b76843034bd197027674f20c1413404d
Noa Misic <nm8625@rit.edu>
2024-03-20 11:17:02 +0000

    Resolve "Level 1: Loading a foods.csv and log.csv file into a model"

--- src/model/DietModel.java
+++ src/model/DietModel.java
@@ -1,8 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+
 public class DietModel {
+
+    private ArrayList<Food> bFoods = new ArrayList<>();
+    private ArrayList<Food> rFoods = new ArrayList<>();
+    private ArrayList<Log> fLog = new ArrayList<>();
+    private ArrayList<Log> wLog = new ArrayList<>();
+
     public DietModel() {
 
+    }
+
+
+    public ArrayList<Food> getbFoods() {
+        return bFoods;
+    }
+
+    public void setbFoods(ArrayList<Food> bFoods) {
+        this.bFoods = bFoods;
+    }
+
+    public ArrayList<Food> getrFoods() {
+        return rFoods;
+    }
+
+    public void setrFoods(ArrayList<Food> rFoods) {
+        this.rFoods = rFoods;
+    }
+
+    public ArrayList<Log> getfLog() {
+        return fLog;
+    }
+
+
+    public void setfLog(ArrayList<Log> fLog) {
+        this.fLog = fLog;
+    }
+
+
+    public ArrayList<Log> getwLog() {
+        return wLog;
+    }
+
+
+    public void setwLog(ArrayList<Log> wLog) {
+        this.wLog = wLog;
     }
 
     //method for getting the Food class
@@ -11,9 +55,20 @@ public class DietModel {
         return f;
     }
 
+    public void getAllFoods(){
+        Food.getData();
+        bFoods = Food.getbFoods();
+        rFoods = Food.getrFoods();
+    }
+
     //method for getting the Log Class
     public Log getLog() {
-        Log l = null;
-        return l;
+        return null;
+    }
+
+    public void getAllLogs(){
+        Log.getData();
+        fLog = Log.getfLog();
+        wLog = Log.getwLog();
     }
 }
--- src/model/Loader.java
+++ src/model/Loader.java
@@ -2,7 +2,9 @@ package model;
 
 public interface Loader {
     // method for getting the data
-    public String getData();
+    public static void getData(){
+        
+    }
 
     // method for setting the data (foods,logs...)
     public void setData(String data);
