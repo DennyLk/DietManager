fc8f8cf07af38543ee8fdc22db83c4dfdce907e0
Denny Lulak <dal4933@rit.edu>
2024-04-11 10:11:06 +0000

    Resolve "Implementing Daily Calorie Goal"

--- src/view/AddUI.java
+++ src/view/AddUI.java
@@ -1,13 +1,10 @@
 package view;
 
-import java.util.ArrayList;
-
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,9 +12,23 @@ public class AddUI implements UI {
     private Stage stage;
     private Scene scene;
     Button backBtn;
+    static TextField calorieGoal;
+    static DatePicker calorieGoalDate;
+    Button logCalorieGoal;
 
     public AddUI(Stage stage) {
         this.stage = stage;
+    }
+
+    public static TextField getCalorieGoal() {
+        return calorieGoal;
+    }
+    public static DatePicker getCalorieGoalDate() {
+        return calorieGoalDate;
+    }
+
+    public Button getLogCalorieGoal() {
+        return logCalorieGoal;
     }
 
     public Button getBackBtn() {
@@ -29,10 +40,15 @@ public class AddUI implements UI {
         stage.setTitle("Add");
 
         VBox root = new VBox(10);
+        Label lbl = new Label("Add Daily Calorie Goal");
+        calorieGoal = new TextField();
+        calorieGoal.setMaxWidth(200);
+        calorieGoalDate = new DatePicker();
+        logCalorieGoal = new Button("Add Daily Calorie Goal");
 
         backBtn = new Button("Back");
 
-        root.getChildren().addAll(backBtn);
+        root.getChildren().addAll(lbl, calorieGoal, calorieGoalDate, logCalorieGoal, backBtn);
 
         scene = new Scene(root, 750, 600);
         stage.setScene(scene);
